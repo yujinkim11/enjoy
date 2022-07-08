1550626e4fc024c7163b9002f5d5c412fc4e7cf9
yujinkim11 <wowyj26@naver.com>
2022-07-08 12:01:12 +0900

    오류 수정중

--- src/conponents/pages/home/BannerMain.js
+++ src/conponents/pages/home/BannerMain.js
@@ -1,9 +1,9 @@
-import styled from "styled-components";
+// import styled from "styled-components";
 
-const BannerMain = styled.div`
-  width: 100%;
-`;
+// // const BannerMain = styled.div`
+//   width: 100%;
+// `;
 
-export const BannerMain = () => {
-  return <div>서브입니당 </div>;
-};
+// export const BannerMain = () => {
+//   return <div>서브입니당 </div>;
+// };
