b03ed0a94250296608317426624075b0809ba48f
yujinkim11 <wowyj26@naver.com>
2022-07-09 00:16:41 +0900

    subpage 1

--- src/conponents/pages/home/BannerMain.js
+++ src/conponents/pages/home/BannerMain.js
@@ -1,11 +1,31 @@
 import styled from "styled-components";
 
-const SubMain = styled.div``;
+const SubMainWrap = styled.div`
+  width: 100%;
+  height: 80vh;
+  background-color: rgba(0, 0, 0, 0.5);
+`;
+
+const ConWrap = styled.div``;
+const Title = styled.div`
+  font-size: 80px;
+  font-weight: 900;
+`;
+const Info = styled.div``;
+const Poster = styled.div``;
+const PlayBox = styled.div``;
 
 export const BannerMain = () => {
   return (
-    <SubMain>
-      <div>어쩌다 사장 올라갈것</div>
-    </SubMain>
+    <SubMainWrap>
+      <ConWrap>
+        <Title>뿅뿅 지구오락실 </Title>
+        <Info>
+          <p>15+ | 예능 | 2022-07-08</p>
+        </Info>
+      </ConWrap>
+      <Poster></Poster>
+      <PlayBox>바로 재생</PlayBox>
+    </SubMainWrap>
   );
 };
