4b36bf1c8770027a1f65e5f27738141ad782f75b
yujinkim11 <wowyj26@naver.com>
2022-07-09 00:43:49 +0900

    round

--- src/conponents/pages/home/BannerMain.js
+++ src/conponents/pages/home/BannerMain.js
@@ -1,10 +1,12 @@
+import { faBookmark, faHeartbeat } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import styled from "styled-components";
 import { mainStyle } from "../../../styles/globalstyle";
 
 const SubMainWrap = styled.div`
   padding: ${mainStyle.padding};
   width: 100%;
-  height: 80vh;
+  height: 75vh;
   background-color: rgba(100, 100, 100, 0.5);
   display: flex;
   align-items: center;
@@ -48,24 +50,68 @@ const PlayBox = styled.div`
   cursor: pointer;
 `;
 
+const RoundWrap = styled.div`
+  padding: ${mainStyle.padding};
+`;
+const RoundTitle = styled.h3`
+  width: 600px;
+  display: flex;
+  font-size: 40px;
+  font-weight: 700;
+  margin: 30px 0 50px 0;
+  h3 {
+    margin-right: 120px;
+  }
+
+  p {
+    margin-right: 20px;
+  }
+`;
+const RoundCon = styled.p`
+  width: 600px;
+  line-height: 40px;
+  font-size: 25px;
+  font-weight: 300;
+  color: rgba(255, 255, 255, 0.7);
+`;
+
 export const BannerMain = () => {
   return (
-    <SubMainWrap>
-      <ConWrap>
-        <Title>뿅뿅 지구오락실 </Title>
-        <Info>
-          <p>15+ | 예능 | 2022-07-08</p>
-        </Info>
-        <Content>
+    <>
+      <SubMainWrap>
+        <ConWrap>
+          <Title>뿅뿅 지구오락실 </Title>
+          <Info>
+            <p>15+ | 예능 | 2022-07-08</p>
+          </Info>
+          <Content>
+            <p>
+              지구로 도망간 달나라 토끼를 잡기위해 뭉친 4명의 용사들! 시공간을
+              넘나들며 펼쳐지는 신개념 하이브리드 멀티버스 액션 어드벤쳐
+              버라이어티가 시작된다!
+            </p>
+          </Content>
+          <PlayBox>바로 재생</PlayBox>
+        </ConWrap>
+        <Poster></Poster>
+      </SubMainWrap>
+      <RoundWrap>
+        <RoundTitle>
+          <h3>2. 뿅뿅 지구오락실 2화</h3>
           <p>
-            지구로 도망간 달나라 토끼를 잡기위해 뭉친 4명의 용사들! 시공간을
-            넘나들며 펼쳐지는 신개념 하이브리드 멀티버스 액션 어드벤쳐
-            버라이어티가 시작된다!
+            <FontAwesomeIcon icon={faHeartbeat} />
           </p>
-        </Content>
-        <PlayBox>바로 재생</PlayBox>
-      </ConWrap>
-      <Poster></Poster>
-    </SubMainWrap>
+          <p>
+            <FontAwesomeIcon icon={faBookmark} />
+          </p>
+        </RoundTitle>
+        <RoundCon>
+          <p>
+            맥주를 부르는 밤 Feat. 테라스 방콕의 밤을 시원~하게 적실 현지
+            맥주&얼음, 각종 안주까지 풀세팅 BUT, 지락실 사전에 공짜는 없다!
+          </p>
+        </RoundCon>
+      </RoundWrap>
+    </>
   );
 };
