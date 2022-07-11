1af4d245766f0f6aa37d34a0a4a318b196c0d240
yujinkim11 <wowyj26@naver.com>
2022-07-11 23:23:31 +0900

    search title

--- src/conponents/pages/search/Search.js
+++ src/conponents/pages/search/Search.js
@@ -1,3 +1,33 @@
+import styled from "styled-components";
+
+const SearchWrap = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+`;
+const SearchTitle = styled.div`
+  margin-top: 30px;
+  text-align: center;
+  h3 {
+    font-size: 60px;
+    font-weight: 900;
+  }
+  p {
+    font-size: 40px;
+    font-weight: 500;
+    color: #707070;
+    margin-top: 30px;
+  }
+`;
+
 export const Search = () => {
-  return <div>검색창</div>;
+  return (
+    <SearchWrap>
+      <SearchTitle>
+        <h3>'어쩌다 사장'</h3>
+        <p>에 관련된 검색 결과입니다.</p>
+      </SearchTitle>
+    </SearchWrap>
+  );
 };
---
+++ src/conponents/pages/search/SearchBox.js
@@ -0,0 +1,7 @@
+import styled from "styled-components";
+
+const SearchBoxWrap = styled.div``;
+
+export const SearchBox = () => {
+  return <SearchBoxWrap></SearchBoxWrap>;
+};
